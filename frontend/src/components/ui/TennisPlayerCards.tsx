252ec199c4fdb5697c49a3a174f7db9d56fc47ea
NSS WebDev <banhazipete@gmail.com>
2025-03-17 18:59:28 +0100

    loading anim ok

--- frontend/src/components/ui/TennisPlayerCards.tsx
+++ frontend/src/components/ui/TennisPlayerCards.tsx
@@ -4,7 +4,8 @@ import { PlayerStats } from '../../utils/types';
 import { TbUserEdit } from 'react-icons/tb';
 import { LuHistory } from 'react-icons/lu';
 import { FaStar } from 'react-icons/fa6';
-import { Spinner } from 'flowbite-react';
+import { RotatingLines } from 'react-loader-spinner';
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import useAxios from '../../utils/useAxios';
 
@@ -153,154 +154,172 @@ const TennisPlayerCards: React.FC<PlayerCardProps> = ({
         <>
             <div
                 // {...containerProps}
-                className={`flex w-[148px] ${
+                className={`flex w-[148px] relative ${
                     isSortable ? 'touch-manipulation' : {}
                 }`}
             >
-                {isLoading ? 'Updating...' : ''}
-                {error && (
-                    <p style={{ color: 'red' }}>Error: {error.message}</p>
-                )}
-                <Card
-                    key={player.name}
-                    className={`flex flex-col w-[148px] h-[290px] border-1 ring-1 ring-inset ring-current transition duration-0 hover:shadow-md scale-100  hover:scale-[1.02] hover:shadow-slate-700 cursor-grab`}
-                    style={
-                        isInCurrentContainer
-                            ? {
-                                  cursor: `default`,
-                                  userSelect: 'none',
-                                  boxShadow:
-                                      '0px 0px 2px rgba(250, 202, 21, 1)',
-                                  transform: 'scale(1.02)',
-                                  backgroundColor: `#${cardColors.mainbackground}`,
-                                  color: `#EAAF51`,
-                              }
-                            : currentCardId == player.id
-                            ? {
-                                  backgroundColor: `#${cardColors.mainbackground}`,
-                                  color: `#EAAF51`,
-                              }
-                            : {
-                                  backgroundColor: `#${cardColors.mainbackground}`,
-                                  color: `#${cardColors.ringcolor}`,
-                              }
-                    }
-                >
-                    <CardHeader className="p-1">
-                        <div className="w-full flex pt-1 pl-1 pr-1 justify-between relative">
-                            <LuHistory className="m-1 hover:cursor-pointer hover:drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-shadow duration-200" />
-
-                            <img
-                                src={player.avatar_url}
-                                alt={player.name}
-                                className="w-16 h-16 rounded-full mx-auto ring-2 ring-current"
-                            />
-                            <div className="absolute top-5 right-24 translate-x-0.5 flex-col items-center justify-center bg-slate-800 rounded-2xl py-0.5 ring-1 ring-slate-500">
-                                <FaStar className="w-2.5 h-2.5 text-yellow-300" />
-                                <FaStar className="w-2.5 h-2.5 text-yellow-300" />
-                                <FaStar className="w-2.5 h-2.5 text-yellow-300" />
-                            </div>
-                            <TbUserEdit className="m-1 hover:cursor-pointer hover:drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-shadow duration-200" />
-                        </div>
-                    </CardHeader>
-                    <div className="flex flex-col justify-between">
-                        <CardContent className="p-1">
-                            <CardTitle
-                                className="mb-2.5 flex font-normal tracking-tighter justify-center"
-                                style={{ color: `#${cardColors.nametext}` }}
-                            >
-                                {player.name}
-                            </CardTitle>
-                            <div className="relative flex flex-col justify-self-center gap-2.5 w-[126px]">
-                                {Object.entries(abilities).map(
-                                    ([skill, value]) => (
-                                        <div
-                                            key={skill}
-                                            className="bg-black/20 flex justify-between h-3.5 ring-1 ring-current rounded-3xl overflow-hidden"
-                                        >
-                                            <div className="capitalize self-center absolute z-10 -translate-y-px">
-                                                <div
-                                                    className="pl-1.5 text-xs"
-                                                    style={{
-                                                        color: `#${cardColors.bartext}`,
-                                                    }}
-                                                >
-                                                    {skill}
-                                                </div>
-                                            </div>
-                                            <div
-                                                className="w-full bg-gray-400 self-center ring-1 ring-current rounded-3xl h-3.5"
-                                                style={{
-                                                    transform: `translateX(${
-                                                        value - 100
-                                                    }%)`,
-                                                }}
-                                            >
-                                                <div
-                                                    className="ring-0 h-3.5 flex rounded-3xl origin-left"
-                                                    style={{
-                                                        backgroundColor: `#${cardColors.barprogress}`,
-                                                    }}
-                                                ></div>
-                                            </div>
-                                            <div className="h-full w-6 ml-1.5 ring-1 ring-current rounded-full bg-black/60">
-                                                <div
-                                                    className="text-xs font-semibold flex justify-center -translate-y-[2px]"
-                                                    style={{
-                                                        color: `#${cardColors.barnumber}`,
-                                                    }}
-                                                >
-                                                    {value}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )
-                                )}
-                            </div>
-                        </CardContent>
-                        <div className="w-full">
-                            <div className="flex text-sm font-normal justify-between m-2.5 pt-0.5">
-                                <button
-                                    className="hover:ring-1 ring-1 hover:cursor-pointer ring-current rounded-xl px-1.5 mb-0.5"
-                                    style={{
-                                        backgroundColor: `#${cardColors.button}`,
-                                    }}
-                                >
-                                    <div
-                                        className="-translate-y-[1px] hover:drop-shadow-[0_0_6px_rgba(255,255,255,1)]"
-                                        style={{
-                                            color: `#${cardColors.buttontext}`,
-                                        }}
-                                        onClick={() =>
-                                            handleToggleFavorite(player.id)
-                                        }
-                                    >
-                                        Favorite
-                                    </div>
-                                </button>
-                                <button
-                                    className="hover:ring-1 ring-1 hover:cursor-pointer ring-current rounded-xl px-1.5 mb-0.5"
-                                    style={{
-                                        backgroundColor: `#${cardColors.button}`,
-                                    }}
-                                    disabled={isLoading}
-                                >
-                                    <div
-                                        className="-translate-y-[1px] hover:drop-shadow-[0_0_6px_rgba(255,255,255,1)]"
-                                        style={{
-                                            color: `#${cardColors.buttontext}`,
-                                        }}
-                                        onClick={() =>
-                                            chooseCurrentPlayer(player.id)
-                                        }
-                                    >
-                                        Choose
-                                    </div>
-                                </button>
+                {isLoading ? (
+                    <div className="flex top-0 left-0 absolute min-h-full min-w-full z-40 border-0 rounded-xl bg-gray-800 bg-opacity-40">
+                        <div className="flex self-center place-content-center min-w-full">
+                            <div className=" w-fit z-41">
+                                <RotatingLines
+                                    width="52px"
+                                    strokeColor="black"
+                                    visible={true}
+                                    animationDuration="1.25"
+                                    ariaLabel="rotating-lines-loading"
+                                />
                             </div>
                         </div>
                     </div>
-                </Card>
+                ) : (
+                    ''
+                )}
+                {error ? (
+                    <p className="text-red-800">Error: {error.message}</p>
+                ) : (
+                    <Card
+                        key={player.name}
+                        className={`flex flex-col w-[148px] h-[290px] border-1 ring-1 ring-inset ring-current transition duration-0 hover:shadow-md scale-100  hover:scale-[1.02] hover:shadow-slate-700 cursor-grab`}
+                        style={
+                            isInCurrentContainer
+                                ? {
+                                      cursor: `default`,
+                                      userSelect: 'none',
+                                      boxShadow:
+                                          '0px 0px 2px rgba(250, 202, 21, 1)',
+                                      //   transform: 'scale(1.02)',
+                                      transform: 'none',
+                                      backgroundColor: `#${cardColors.mainbackground}`,
+                                      color: `#EAAF51`,
+                                  }
+                                : currentCardId == player.id
+                                ? {
+                                      backgroundColor: `#${cardColors.mainbackground}`,
+                                      color: `#EAAF51`,
+                                  }
+                                : {
+                                      backgroundColor: `#${cardColors.mainbackground}`,
+                                      color: `#${cardColors.ringcolor}`,
+                                  }
+                        }
+                    >
+                        <CardHeader className="p-1">
+                            <div className="w-full flex pt-1 pl-1 pr-1 justify-between relative">
+                                <LuHistory className="m-1 hover:cursor-pointer hover:drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-shadow duration-200" />
+
+                                <img
+                                    src={player.avatar_url}
+                                    alt={player.name}
+                                    className="w-16 h-16 rounded-full mx-auto ring-2 ring-current"
+                                />
+                                <div className="absolute top-5 right-24 translate-x-0.5 flex-col items-center justify-center bg-slate-800 rounded-2xl py-0.5 ring-1 ring-slate-500">
+                                    <FaStar className="w-2.5 h-2.5 text-yellow-300" />
+                                    <FaStar className="w-2.5 h-2.5 text-yellow-300" />
+                                    <FaStar className="w-2.5 h-2.5 text-yellow-300" />
+                                </div>
+                                <TbUserEdit className="m-1 hover:cursor-pointer hover:drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-shadow duration-200" />
+                            </div>
+                        </CardHeader>
+                        <div className="flex flex-col justify-between">
+                            <CardContent className="p-1">
+                                <CardTitle
+                                    className="mb-2.5 flex font-normal tracking-tighter justify-center"
+                                    style={{ color: `#${cardColors.nametext}` }}
+                                >
+                                    {player.name}
+                                </CardTitle>
+                                <div className="relative flex flex-col justify-self-center gap-2.5 w-[126px]">
+                                    {Object.entries(abilities).map(
+                                        ([skill, value]) => (
+                                            <div
+                                                key={skill}
+                                                className="bg-black/20 flex justify-between h-3.5 ring-1 ring-current rounded-3xl overflow-hidden"
+                                            >
+                                                <div className="capitalize self-center absolute z-10 -translate-y-px">
+                                                    <div
+                                                        className="pl-1.5 text-xs"
+                                                        style={{
+                                                            color: `#${cardColors.bartext}`,
+                                                        }}
+                                                    >
+                                                        {skill}
+                                                    </div>
+                                                </div>
+                                                <div
+                                                    className="w-full bg-gray-400 self-center ring-1 ring-current rounded-3xl h-3.5"
+                                                    style={{
+                                                        transform: `translateX(${
+                                                            value - 100
+                                                        }%)`,
+                                                    }}
+                                                >
+                                                    <div
+                                                        className="ring-0 h-3.5 flex rounded-3xl origin-left"
+                                                        style={{
+                                                            backgroundColor: `#${cardColors.barprogress}`,
+                                                        }}
+                                                    ></div>
+                                                </div>
+                                                <div className="h-full w-6 ml-1.5 ring-1 ring-current rounded-full bg-black/60">
+                                                    <div
+                                                        className="text-xs font-semibold flex justify-center -translate-y-[2px]"
+                                                        style={{
+                                                            color: `#${cardColors.barnumber}`,
+                                                        }}
+                                                    >
+                                                        {value}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            </CardContent>
+                            <div className="w-full">
+                                <div className="flex text-sm font-normal justify-between m-2.5 pt-0.5">
+                                    <button
+                                        className="hover:ring-1 ring-1 hover:cursor-pointer ring-current rounded-xl px-1.5 mb-0.5"
+                                        style={{
+                                            backgroundColor: `#${cardColors.button}`,
+                                        }}
+                                    >
+                                        <div
+                                            className="-translate-y-[1px] hover:drop-shadow-[0_0_6px_rgba(255,255,255,1)]"
+                                            style={{
+                                                color: `#${cardColors.buttontext}`,
+                                            }}
+                                            onClick={() =>
+                                                handleToggleFavorite(player.id)
+                                            }
+                                        >
+                                            Favorite
+                                        </div>
+                                    </button>
+                                    <button
+                                        className="hover:ring-1 ring-1 hover:cursor-pointer ring-current rounded-xl px-1.5 mb-0.5"
+                                        style={{
+                                            backgroundColor: `#${cardColors.button}`,
+                                        }}
+                                        disabled={isLoading}
+                                    >
+                                        <div
+                                            className="-translate-y-[1px] hover:drop-shadow-[0_0_6px_rgba(255,255,255,1)]"
+                                            style={{
+                                                color: `#${cardColors.buttontext}`,
+                                            }}
+                                            onClick={() =>
+                                                chooseCurrentPlayer(player.id)
+                                            }
+                                        >
+                                            Choose
+                                        </div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                )}
             </div>
         </>
     );
