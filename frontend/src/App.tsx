bb67c093bef0530abf2a12f93f8c6d152aab6ec9
NSS WebDev <banhazipete@gmail.com>
2025-05-15 18:52:40 +0200

    dashboardlayout removed app.tsx refacted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,26 +3,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import MainWrapper from './routsandwrappers/MainAuthWrapper';
 
-import Register from './features/app/register';
-
-import WebSocketChat from './temp/wstest';
-import ChatComponent from './components/ChatComponent';
-
 import Navbar from './pages/Navbar';
 
 import BottomFooter from './pages/BottomFooter';
 import MainDesignWrapper from './routsandwrappers/MainDesignWrapper';
 
-import DashboardMainLayout from './routsandwrappers/DashboardMainLayout';
 import { Notifications } from './components/ui/notifications';
 import Landing from './pages/Landing';
-import Login from './features/app/login';
+
 import Logout from './features/app/logout';
 import TopList from './features/ranks/Ranks';
 import PrivateRoute from './routsandwrappers/PrivateRoute';
 import Options from './features/options/Options';
 import ChatLobby from './pages/ChatLobby';
 import DashboardManager from './features/dashboard/DashboardManager';
+import GameWrapper from './pages/GameWrapper';
+import Rules from './pages/Rules';
 
 const queryClient = new QueryClient();
 
@@ -57,11 +53,42 @@ const App: React.FC = () => {
                                             </DashboardManager>
                                         </PrivateRoute>
                                     }
+                                />{' '}
+                                <Route
+                                    path="/lobby"
+                                    element={
+                                        <PrivateRoute>
+                                            <DashboardManager>
+                                                <ChatLobby />
+                                            </DashboardManager>
+                                        </PrivateRoute>
+                                    }
                                 />
                                 <Route
-                                    path="*"
-                                    element={<DashboardMainLayout />}
+                                    path="/matches"
+                                    element={
+                                        <DashboardManager>
+                                            <GameWrapper />
+                                        </DashboardManager>
+                                    }
                                 />
+                                <Route
+                                    path="/ranks"
+                                    element={
+                                        <DashboardManager>
+                                            <TopList />
+                                        </DashboardManager>
+                                    }
+                                />
+                                <Route
+                                    path="/rules"
+                                    element={
+                                        <DashboardManager>
+                                            <Rules />
+                                        </DashboardManager>
+                                    }
+                                />
+                                <Route path="*" element={<Landing />} />
                                 {/* <Route path="/login" element={<Login />} />
                                 <Route
                                     path="/register"
